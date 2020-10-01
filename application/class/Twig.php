fb084608b9abb91223f85c566fc90885e76682be
fouad-git <fouad.lyousfi@hotmail.com>
2020-10-01 18:51:38 +0200

    first commit
    add file Database and twig

---
+++ application/class/Twig.php
@@ -0,0 +1,26 @@
+<?php
+namespace App;
+
+
+class Twig{
+    // avant private $template;
+    private $template;
+
+    public function __construct($template_name)
+    {
+        $loader = new \Twig\Loader\FilesystemLoader(dirname(dirname(__FILE__)).'/template');
+        $twig = new \Twig\Environment($loader, [
+            'cache' => '../cache',
+            'debug' => true,
+        ]);
+        $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+        $this->template = $twig->load($template_name);
+    }
+
+    public function render($arr=[])
+    {
+        echo $this->template->render($arr);
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once  dirname(dirname(__FILE__)).'/vendor/autoload.php';
+define('BASE_PATH', '/petites-annonces/public');
+// pour passer à twig pour avoir les bonnes adresses à (mettre dans le render)
+define('SERVER_URI', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME']/*.':'.$_SERVER['REMOTE_PORT']*/.BASE_PATH);
+
+// pour initialiser altorouter
+$router = new AltoRouter();
+$router->setBasePath(BASE_PATH);
+
+
+
+
+$match = $router->match();
+   
+// fermeture d'appel ou lance le status 404
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] );
+} else {
+	// Aucune route ne correspond
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+?>
